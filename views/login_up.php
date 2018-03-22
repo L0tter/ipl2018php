7692318b8e7d9ab1f0fbdd99f5f5c4ad6058ec4f
L0tter <31412100+L0tter@users.noreply.github.com>
2018-03-22 12:40:49 +0100

    some views
    home, et login embryonnaire

---
+++ views/login_up.php
@@ -0,0 +1,16 @@
+<article>
+    <form action="index.php" method="POST">
+        <legend>Inscription</legend>
+        <label> Nom : </label> <br>
+        <label> Prenom : </label> <br>
+    </form>
+</article>
+<article>
+    <form action="index.php" method="POST">
+        <legend>Connexion</legend>
+        <label>Pseudo : </pseudo><input type="text" name="user" required>
+        <label>Mot de passe : </label><input type="password" name="passwd" required>
+        <input type="submit" name="LOGIN" value="Connexion">
+    </form>
+</article>
+
