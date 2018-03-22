e47f92d25f147c05f41bd48cc2822c82df981f62
razzia <alexk-m@outlook.com>
2018-03-22 20:52:39 +0100

    Modification home (connexion/inscription)

--- views/login_up.php
+++ views/login_up.php
@@ -1,15 +1,20 @@
 <article>
+	<h2> Home Page !</h2>
+    <p>Bienvenue sur la page Home</p>
+    <form action="index.php" method="POST">
+        <input type="submit" name="logINUP" value="Connexion/Inscription">
+    </form>
     <form action="index.php" method="POST">
         <legend>Inscription</legend>
-        <label> Nom : </label> <br>
-        <label> Prenom : </label> <br>
+        <label> Nom : </label> <input type="text" name="lname" required><br>
+        <label> Prenom : </label> <input type="text" name="fname" required><br>
     </form>
 </article>
 <article>
     <form action="index.php" method="POST">
         <legend>Connexion</legend>
-        <label>Pseudo : </pseudo><input type="text" name="user" required>
-        <label>Mot de passe : </label><input type="password" name="passwd" required>
+        <label>Pseudo : </label><input type="text" name="user" required></br>
+        <label>Mot de passe : </label><input type="password" name="passwd" required></br>
         <input type="submit" name="LOGIN" value="Connexion">
     </form>
 </article>
