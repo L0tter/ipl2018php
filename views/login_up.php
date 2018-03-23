67fcd6885fd2bce5d0e2520e0147cc4db96aedb7
L0tter <31412100+L0tter@users.noreply.github.com>
2018-03-23 10:54:34 +0100

    loginup fix

--- views/login_up.php
+++ views/login_up.php
@@ -1,9 +1,4 @@
 <article>
-	<h2> Home Page !</h2>
-    <p>Bienvenue sur la page Home</p>
-    <form action="index.php" method="POST">
-        <input type="submit" name="logINUP" value="Connexion/Inscription">
-    </form>
     <form action="index.php" method="POST">
         <legend>Inscription</legend>
         <label> Nom : </label> <input type="text" name="lname" required><br>
